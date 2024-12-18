478b942a81c3e12457d304862a4558ee77827167
Péter Kelemen <peter.kelemen456@gmail.com>
2024-12-18 01:37:37 +0100

    Removed long comment from main

--- GameplayTimeTracker/MainWindow.xaml.cs
+++ GameplayTimeTracker/MainWindow.xaml.cs
@@ -50,36 +50,6 @@ namespace GameplayTimeTracker
             UpdateStackPane();
             tileContainer.Total = GamesLoaded;
             GamesLoaded.Text = $"Games managed: {tileContainer.tilesList.Count}";
-
-            // Console.WriteLine(Utils.ConvertMinutesToTime(108));
-            // Console.WriteLine(Utils.ConvertMinutesToTime(20));
-            //
-            // Console.WriteLine(Utils.ConvertMinutesToTime(875));
-            // Console.WriteLine(Utils.ConvertMinutesToTime(1));
-            //
-            // Console.WriteLine(Utils.ConvertMinutesToTime(119));
-            // Console.WriteLine(Utils.ConvertMinutesToTime(1));
-            //
-            // Console.WriteLine(Utils.ConvertMinutesToTime(350));
-            // Console.WriteLine(Utils.ConvertMinutesToTime(151));
-            //
-            // Console.WriteLine(Utils.ConvertMinutesToTime(2144));
-            // Console.WriteLine(Utils.ConvertMinutesToTime(48));
-            //
-            // Console.WriteLine(Utils.ConvertMinutesToTime(60));
-            // Console.WriteLine(Utils.ConvertMinutesToTime(60));
-            //
-            // Console.WriteLine(Utils.ConvertMinutesToTime(21));
-            // Console.WriteLine(Utils.ConvertMinutesToTime(1));
-            //
-            // Console.WriteLine(Utils.ConvertMinutesToTime(256));
-            // Console.WriteLine(Utils.ConvertMinutesToTime(163));
-            //
-            // Console.WriteLine(Utils.ConvertMinutesToTime(180));
-            // Console.WriteLine(Utils.ConvertMinutesToTime(0));
-            //
-            // Console.WriteLine(Utils.ConvertMinutesToTime(99));
-            // Console.WriteLine(Utils.ConvertMinutesToTime(39));
         }
 
         private void InitSettings()
