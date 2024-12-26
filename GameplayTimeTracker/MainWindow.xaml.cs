1d7266a26d64642667be85645afd8efcf0345fd2
Péter Kelemen <peter.kelemen456@gmail.com>
2024-12-27 00:35:32 +0100

    Optimized imports

--- GameplayTimeTracker/MainWindow.xaml.cs
+++ GameplayTimeTracker/MainWindow.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
-using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
