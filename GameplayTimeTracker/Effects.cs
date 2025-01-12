b91f46a6187269ccc429bd84aafc8c127c51c166
Péter Kelemen <peter.kelemen456@gmail.com>
2025-01-12 23:56:16 +0100

    Added class for storing global effects

---
+++ GameplayTimeTracker/Effects.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Effects;
+
+namespace GameplayTimeTracker;
+
+public static class Effects
+{
+    public static BlurEffect fakeShadow = new BlurEffect
+    {
+        Radius = 8,
+    };
+
+    public static BlurEffect blurEffect = new BlurEffect
+    {
+        Radius = 10,
+        RenderingBias = RenderingBias.Performance
+    };
+
+    public static BlurEffect bigBlurEffect = new BlurEffect
+    {
+        Radius = 30,
+        RenderingBias = RenderingBias.Performance
+    };
+
+    public static OuterGlowBitmapEffect outerGlowEffect = new OuterGlowBitmapEffect
+    {
+        GlowSize = 10
+    };
+
+    public static DropShadowEffect dropShadowText = new DropShadowEffect
+    {
+        BlurRadius = 8,
+        ShadowDepth = 0,
+        Color = Colors.Black,
+        Opacity = 1,
+        Direction = 200,
+    };
+
+    public static DropShadowEffect dropShadowTextEdit = new DropShadowEffect
+    {
+        BlurRadius = 5,
+        ShadowDepth = 0,
+        Color = Colors.LightBlue,
+        Opacity = 1,
+        Direction = 200,
+    };
+
+    public static DropShadowEffect dropShadowIcon = new DropShadowEffect
+    {
+        BlurRadius = 10,
+        ShadowDepth = 0
+    };
+
+    public static DropShadowEffect dropShadowLightArea = new DropShadowEffect
+    {
+        BlurRadius = 7,
+        ShadowDepth = 0
+    };
+
+    public static DropShadowEffect dropShadowRectangle = new DropShadowEffect
+    {
+        BlurRadius = 20,
+        ShadowDepth = 0
+    };
+
+    public static DoubleAnimation dragFadeInAnimation = new DoubleAnimation
+    {
+        From = 0,
+        To = 1,
+        Duration = new Duration(TimeSpan.FromSeconds(0.2)),
+        EasingFunction = new CubicEase { EasingMode = EasingMode.EaseOut }
+    };
+
+    public static DoubleAnimation dragFadeOutAnimation = new DoubleAnimation
+    {
+        From = 1,
+        To = 0,
+        Duration = new Duration(TimeSpan.FromSeconds(0.2)),
+        EasingFunction = new CubicEase { EasingMode = EasingMode.EaseOut }
+    };
+}
