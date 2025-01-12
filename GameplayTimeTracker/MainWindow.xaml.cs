0d7d3f985a32375274d7c7cf859430ed7b569312
Péter Kelemen <peter.kelemen456@gmail.com>
2025-01-12 23:29:37 +0100

    Restored some layout changes

--- GameplayTimeTracker/MainWindow.xaml.cs
+++ GameplayTimeTracker/MainWindow.xaml.cs
@@ -11,13 +11,15 @@ using System.Windows.Shapes;
 
 namespace GameplayTimeTracker;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
+        Loaded += OnLoaded;
+    }
+
+    public void OnLoaded(object sender, RoutedEventArgs e)
+    {
     }
 }
