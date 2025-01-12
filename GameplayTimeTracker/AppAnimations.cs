80d5110d9800afc4738d318116b28a360c46fcc0
Péter Kelemen <peter.kelemen456@gmail.com>
2025-01-13 00:15:03 +0100

    Added class for application animations

---
+++ GameplayTimeTracker/AppAnimations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace GameplayTimeTracker;
+
+public class AppAnimations
+{
+    public static DoubleAnimation dragFadeInAnimation = new DoubleAnimation
+    {
+        From = 0,
+        To = 1,
+        Duration = new Duration(TimeSpan.FromSeconds(0.2)),
+        EasingFunction = new CubicEase { EasingMode = EasingMode.EaseOut }
+    };
+
+    public static DoubleAnimation dragFadeOutAnimation = new DoubleAnimation
+    {
+        From = 1,
+        To = 0,
+        Duration = new Duration(TimeSpan.FromSeconds(0.2)),
+        EasingFunction = new CubicEase { EasingMode = EasingMode.EaseOut }
+    };
+}
--- GameplayTimeTracker/Effects.cs
+++ GameplayTimeTracker/Effects.cs
@@ -65,20 +65,4 @@ public static class Effects
         BlurRadius = 20,
         ShadowDepth = 0
     };
-
-    public static DoubleAnimation dragFadeInAnimation = new DoubleAnimation
-    {
-        From = 0,
-        To = 1,
-        Duration = new Duration(TimeSpan.FromSeconds(0.2)),
-        EasingFunction = new CubicEase { EasingMode = EasingMode.EaseOut }
-    };
-
-    public static DoubleAnimation dragFadeOutAnimation = new DoubleAnimation
-    {
-        From = 1,
-        To = 0,
-        Duration = new Duration(TimeSpan.FromSeconds(0.2)),
-        EasingFunction = new CubicEase { EasingMode = EasingMode.EaseOut }
-    };
 }
