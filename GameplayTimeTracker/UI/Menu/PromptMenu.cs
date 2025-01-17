2671d39d9b2f9f4671826ccf54fe29c85b476b8f
Péter Kelemen <peter.kelemen456@gmail.com>
2025-01-17 16:03:48 +0100

    Started adding prompt menu

---
+++ GameplayTimeTracker/UI/Menu/PromptMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace GameplayTimeTracker.Menu;
+
+public class PromptMenu : CustomMenu
+{
+    public enum PromptType
+    {
+        YesNo,
+        Ok
+    }
+
+    private RoutedEventHandler _yesHandler;
+    private RoutedEventHandler _noHandler;
+
+    public PromptMenu(string[] text, double width = 300, double height = 400, PromptType type = PromptType.Ok,
+        RoutedEventHandler yesHandler = null, RoutedEventHandler noHandler = null, bool performanceMode = true)
+        : base(width, height, performanceMode)
+    {
+        
+    }
+}
