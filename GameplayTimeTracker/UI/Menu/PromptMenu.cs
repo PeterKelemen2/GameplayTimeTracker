54771d05258ecf674b4194d3bfbfc006ef634a59
Péter Kelemen <peter.kelemen456@gmail.com>
2025-01-17 17:25:37 +0100

    Refactoring

--- GameplayTimeTracker/UI/Menu/PromptMenu.cs
+++ GameplayTimeTracker/UI/Menu/PromptMenu.cs
@@ -9,87 +9,65 @@ namespace GameplayTimeTracker.Menu;
 
 public class PromptMenu : CustomMenu
 {
-    public enum PromptType
-    {
-        YesNo,
-        Ok
-    }
+    public enum PromptType { YesNo, Ok }
 
-    public PromptMenu(string[] textArray, double[] sizeArray = null, bool[] boldArray = null, double spaceBetween = 0,
-        double width = 300, double height = 200,
-        PromptType type = PromptType.Ok,
+    public PromptMenu(
+        string[] textArray, double[] sizeArray = null, bool[] boldArray = null, double lineSpacing = 0,
+        double width = 300, double height = 200, PromptType type = PromptType.Ok,
         RoutedEventHandler yesHandler = null, RoutedEventHandler noHandler = null,
         bool performanceMode = true)
         : base(width, height, performanceMode)
     {
-        sizeArray = SetSizeArray(sizeArray, textArray);
-        boldArray = SetBoldArray(boldArray, textArray);
+        sizeArray = (sizeArray ?? new double[0])
+            .Concat(Enumerable.Repeat(Common.EditTitleFontSize, Math.Max(0, textArray.Length - (sizeArray?.Length ?? 0))))
+            .ToArray();
+        boldArray = Enumerable.Range(0, textArray.Length)
+            .Select(i => boldArray != null && i < boldArray.Length && boldArray[i])
+            .ToArray();
 
-        TextBlock promptTextBlock = new TextBlock
+        var promptTextBlock = new TextBlock
         {
             TextAlignment = TextAlignment.Center,
             Margin = new Thickness(0, 20, 0, 0),
             Foreground = new SolidColorBrush(AppColors.Font),
-            TextWrapping = TextWrapping.Wrap,
+            TextWrapping = TextWrapping.Wrap
         };
+
         for (int i = 0; i < textArray.Length; i++)
         {
             promptTextBlock.Inlines.Add(new Run
             {
                 Text = textArray[i] + "\n",
                 FontSize = sizeArray[i],
-                FontWeight = boldArray[i] ? FontWeights.Bold : FontWeights.Regular,
+                FontWeight = boldArray[i] ? FontWeights.Bold : FontWeights.Regular
             });
-            promptTextBlock.Inlines.Add(new Run
-                { Text = ".\n", Foreground = Brushes.Transparent, FontSize = spaceBetween });
+            promptTextBlock.Inlines.Add(new Run { Text = ".\n", Foreground = Brushes.Transparent, FontSize = lineSpacing });
         }
-
         MenuContentGrid.Children.Add(promptTextBlock);
 
-        switch (type)
+        if (type == PromptType.YesNo)
         {
-            case PromptType.YesNo:
-                double margin = 120;
-                var yesButton = new CustomButton(text: "Yes", width: 100, height: 35, vA: VerticalAlignment.Bottom,
-                    type: CustomButton.ButtonType.Negative, effect: AppEffects.DropShadowMedium);
-                yesButton.Margin = new Thickness(0, 0, margin, 20);
-                var noButton = new CustomButton(text: "No", width: 100, height: 35, vA: VerticalAlignment.Bottom,
-                    type: CustomButton.ButtonType.Positive, effect: AppEffects.DropShadowMedium);
-                noButton.Margin = new Thickness(margin, 0, 0, 20);
-                
-                yesButton.Click += (s, e) =>
-                {
-                    (yesHandler ?? ((s, e) => Console.WriteLine("No REH")))(s, e);
-                    Close();
-                };
-                noButton.Click += (s, e) =>
-                {
-                    (noHandler ?? ((s, e) => Console.WriteLine("No REH")))(s, e);
-                    Close();
-                };
-                
-                MenuContentGrid.Children.Add(yesButton);
-                MenuContentGrid.Children.Add(noButton);
-                break;
-            case PromptType.Ok:
-                var okButton = new CustomButton(text: "Ok", width: 100, height: 35, hA: HorizontalAlignment.Center,
-                    vA: VerticalAlignment.Bottom, effect: AppEffects.DropShadowMedium);
-                okButton.Margin = new Thickness(0, 0, 0, 20);
-                okButton.Click += (s, e) => { Close(); };
-                MenuContentGrid.Children.Add(okButton);
-                break;
-            default:
-                break;
+            AddButton("Yes", 60, yesHandler, CustomButton.ButtonType.Positive);
+            AddButton("No", -60, noHandler, CustomButton.ButtonType.Negative);
+        }
+        else
+        {
+            AddButton("Ok", 0, null, CustomButton.ButtonType.Default);
         }
     }
 
-    private double[] SetSizeArray(double[] sizeArray, string[] textArray) =>
-        (sizeArray ?? new double[0])
-        .Concat(Enumerable.Repeat(Common.EditTitleFontSize, Math.Max(0, textArray.Length - (sizeArray?.Length ?? 0))))
-        .ToArray();
-
-    private bool[] SetBoldArray(bool[] boldArray, string[] textArray) =>
-        Enumerable.Range(0, textArray.Length)
-            .Select(i => boldArray != null && i < boldArray.Length && boldArray[i])
-            .ToArray();
+    private void AddButton(string text, double horizontalMargin, RoutedEventHandler handler, CustomButton.ButtonType buttonType)
+    {
+        var button = new CustomButton(text: text, width: 100, height: 35, hA: HorizontalAlignment.Center,
+            vA: VerticalAlignment.Bottom, type: buttonType, effect: AppEffects.DropShadowMedium)
+        {
+            Margin = new Thickness(-horizontalMargin, 0, horizontalMargin, 20)
+        };
+        button.Click += (s, e) =>
+        {
+            (handler ?? ((s, e) => Console.WriteLine($"No Event set for {text} Button")))(s, e);
+            Close();
+        };
+        MenuContentGrid.Children.Add(button);
+    }
 }
