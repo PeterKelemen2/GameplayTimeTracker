cc6471b8cb777214763f68ef684b5d7d85695b41
Péter Kelemen <peter.kelemen456@gmail.com>
2025-01-18 05:23:36 +0100

    Added bindigs to most text boxes

--- GameplayTimeTracker/UI/Menu/EditMenu.cs
+++ GameplayTimeTracker/UI/Menu/EditMenu.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Media;
 
@@ -46,60 +47,63 @@ public class EditMenu : CustomMenu
             { Width = MenuContentBg.Width, Height = bHeight + 25, HorizontalAlignment = HorizontalAlignment.Left };
         TextBlock editNameTextBlock = CreateTextBlock("Name");
         TextBox editNameTextBox = CreateTextBox(Entry.Name);
-        var BrowseExeButton = new CustomButton(text: "New exe", width: bWidth, height: bHeight,
-            buttonImagePath: AppFiles.AddIcon,
+        var ChangeIconButton = new CustomButton(text: "Change Icon", width: bWidth, height: bHeight,
+            buttonImagePath: AppFiles.EditIcon, effect: AppEffects.DropShadowMedium,
             hA: HorizontalAlignment.Right, vA: VerticalAlignment.Bottom);
-        BrowseExeButton.Effect = AppEffects.DropShadowMedium;
-        BrowseExeButton.Margin = new Thickness(0, 5, LeftMargin, 5);
-        BrowseExeButton.Click += (_, _) => { EditHelper.UpdateExe(entry); };
+        ChangeIconButton.Margin = new Thickness(0, 5, LeftMargin, 5);
+        Binding nameBinding = new Binding("Name") { Source = Entry, Mode = BindingMode.TwoWay, };
+        BindingOperations.SetBinding(editNameTextBox, TextBox.TextProperty, nameBinding);
+
         gridName.Children.Add(editNameTextBlock);
         gridName.Children.Add(editNameTextBox);
-        gridName.Children.Add(BrowseExeButton);
+        gridName.Children.Add(ChangeIconButton);
         StackPanel.Children.Add(gridName);
 
         Grid gridTime = new Grid
             { Width = MenuContentBg.Width, Height = bHeight + 25, HorizontalAlignment = HorizontalAlignment.Left };
         TextBlock editTimeTextBlock = CreateTextBlock("Playtime");
         TextBox editTimeTextBox = CreateTextBox(Entry.TotalPlayFormatted);
-        var OpenFolderButton = new CustomButton(text: "Open Folder", width: bWidth, height: bHeight,
-            buttonImagePath: AppFiles.FolderIcon,
+
+        var ChangeHeroButton = new CustomButton(text: "Change Hero", width: bWidth, height: bHeight,
+            buttonImagePath: AppFiles.EditIcon, effect: AppEffects.DropShadowMedium,
             hA: HorizontalAlignment.Right, vA: VerticalAlignment.Bottom);
-        OpenFolderButton.Effect = AppEffects.DropShadowMedium;
-        OpenFolderButton.Margin = new Thickness(0, 5, LeftMargin, 5);
-        OpenFolderButton.Click += (s, e) => { Process.Start("explorer.exe", $"/select,\"{Entry.ExePath}\""); };
+        ChangeHeroButton.Margin = new Thickness(0, 5, LeftMargin, 5);
         gridTime.Children.Add(editTimeTextBlock);
         gridTime.Children.Add(editTimeTextBox);
-        gridTime.Children.Add(OpenFolderButton);
+        gridTime.Children.Add(ChangeHeroButton);
         StackPanel.Children.Add(gridTime);
 
         Grid gridPath = new Grid
             { Width = MenuContentBg.Width, Height = bHeight + 25, HorizontalAlignment = HorizontalAlignment.Left };
         TextBlock editPathTextBlock = CreateTextBlock("Path");
         TextBox editPathTextBox = CreateTextBox(Entry.ExePath);
-        var ChangeIconButton = new CustomButton(text: "Change Icon", width: bWidth, height: bHeight,
-            buttonImagePath: AppFiles.EditIcon,
+        Binding exeBinding = new Binding("ExePath") { Source = Entry, Mode = BindingMode.TwoWay, };
+        BindingOperations.SetBinding(editPathTextBox, TextBox.TextProperty, exeBinding);
+        var BrowseExeButton = new CustomButton(text: "New exe", width: bWidth, height: bHeight,
+            buttonImagePath: AppFiles.AddIcon, effect: AppEffects.DropShadowMedium,
             hA: HorizontalAlignment.Right, vA: VerticalAlignment.Bottom);
-        ChangeIconButton.Margin = new Thickness(0, 5, LeftMargin, 5);
-        ChangeIconButton.Effect = AppEffects.DropShadowMedium;
+        BrowseExeButton.Margin = new Thickness(0, 5, LeftMargin, 5);
+        BrowseExeButton.Click += (_, _) => { EditHelper.UpdateExe(entry); };
         // ChangeIconButton.Click += Parent.UpdateIcons;
         gridPath.Children.Add(editPathTextBlock);
         gridPath.Children.Add(editPathTextBox);
-        gridPath.Children.Add(ChangeIconButton);
+        gridPath.Children.Add(BrowseExeButton);
         StackPanel.Children.Add(gridPath);
 
         Grid gridArguments = new Grid
             { Width = MenuContentBg.Width, Height = bHeight + 25, HorizontalAlignment = HorizontalAlignment.Left };
         TextBlock editArgsTextBlock = CreateTextBlock("Arguments");
         TextBox editArgsTextBox = CreateTextBox(Entry.Arguments);
-        var ChangeHeroButton = new CustomButton(text: "Change Hero", width: bWidth, height: bHeight,
-            buttonImagePath: AppFiles.EditIcon,
+        Binding argsBinding = new Binding("Arguments") { Source = Entry, Mode = BindingMode.TwoWay, };
+        BindingOperations.SetBinding(editArgsTextBox, TextBox.TextProperty, argsBinding);
+        var OpenFolderButton = new CustomButton(text: "Open Folder", width: bWidth, height: bHeight,
+            buttonImagePath: AppFiles.FolderIcon, effect: AppEffects.DropShadowMedium,
             hA: HorizontalAlignment.Right, vA: VerticalAlignment.Bottom);
-        ChangeHeroButton.Margin = new Thickness(0, 5, LeftMargin, 5);
-        ChangeHeroButton.Effect = AppEffects.DropShadowMedium;
-        // SaveButton.Click += Parent.editSaveButton_Click;
+        OpenFolderButton.Margin = new Thickness(0, 5, LeftMargin, 5);
+        OpenFolderButton.Click += (s, e) => { Process.Start("explorer.exe", $"/select,\"{Entry.ExePath}\""); };
         gridArguments.Children.Add(editArgsTextBlock);
         gridArguments.Children.Add(editArgsTextBox);
-        gridArguments.Children.Add(ChangeHeroButton);
+        gridArguments.Children.Add(OpenFolderButton);
         StackPanel.Children.Add(gridArguments);
 
         var SaveButton = new CustomButton(text: "Save", width: bWidth, height: bHeight,
