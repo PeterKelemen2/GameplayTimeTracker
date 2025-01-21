70500d453e5c7b9ffd02bf04093a5e99215db22e
Péter Kelemen <peter.kelemen456@gmail.com>
2025-01-21 09:52:08 +0100

    Added settings class

---
+++ GameplayTimeTracker/Core/Settings/AppSettings.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace GameplayTimeTracker.Settings;
+
+public class AppSettings
+{
+    [JsonPropertyName("Start with System")]
+    public bool StartWithSystem { get; set; } = true;
+
+    [JsonPropertyName("SteamGridDB API key")]
+    public string SGDBApiKey { get; set; } = string.Empty;
+
+    [JsonPropertyName("Prefer Local App Image")]
+    public bool PreferLocalAppImage { get; set; } = false;
+}
