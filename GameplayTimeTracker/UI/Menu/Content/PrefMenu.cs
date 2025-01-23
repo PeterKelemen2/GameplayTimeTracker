685e020075322dff0a831132ea2b6e240484e9f2
Péter Kelemen <peter.kelemen456@gmail.com>
2025-01-23 10:49:39 +0100

    Added binding to preferences menu

--- GameplayTimeTracker/UI/Menu/Content/PrefMenu.cs
+++ GameplayTimeTracker/UI/Menu/Content/PrefMenu.cs
@@ -15,23 +15,25 @@ public class PrefMenu : UserControl
     {
         Panel = new StackPanel();
         PrefEntry pref1 = new PrefEntry("Start With System", settings.StartWithSystem);
-        Binding swsBinding = new Binding("StartWithSystem")
-        {
-            Source = settings,
-            Mode = BindingMode.TwoWay,
-        };
+        Binding swsBinding = new Binding("StartWithSystem") { Source = settings, Mode = BindingMode.TwoWay, };
         BindingOperations.SetBinding(pref1.checkBox, CheckBox.IsCheckedProperty, swsBinding);
-
         Panel.Children.Add(pref1);
 
         PrefEntry pref2 = new PrefEntry("Prefer SteamGridDB Images", settings.PreferSteamGridDBImage);
+        Binding preferSGDBBinding = new Binding("PreferSteamGridDBImage")
+            { Source = settings, Mode = BindingMode.TwoWay, };
+        BindingOperations.SetBinding(pref2.checkBox, CheckBox.IsCheckedProperty, preferSGDBBinding);
         Panel.Children.Add(pref2);
 
         TextBox sgdbApiKeyBox = UIHelper.CreateTextBox(placeholder: "API Key");
+        Binding sgdbApiKeyBinding = new Binding("SGDBApiKey") { Source = settings, Mode = BindingMode.TwoWay, };
+        BindingOperations.SetBinding(sgdbApiKeyBox, TextBox.TextProperty, sgdbApiKeyBinding);
         sgdbApiKeyBox.Margin = new Thickness(40, 0, 0, 0);
         Panel.Children.Add(sgdbApiKeyBox);
 
         PrefEntry pref3 = new PrefEntry("Quick Add", settings.QuickAdd);
+        Binding quickAddBinding = new Binding("QuickAdd") { Source = settings, Mode = BindingMode.TwoWay, };
+        BindingOperations.SetBinding(pref3.checkBox, CheckBox.IsCheckedProperty, quickAddBinding);
         Panel.Children.Add(pref3);
     }
 }
