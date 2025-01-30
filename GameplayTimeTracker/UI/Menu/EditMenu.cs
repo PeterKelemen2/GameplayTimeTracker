aba7a4d45e53b705ca66a10b7e0a76ffeac7fa4d
Péter Kelemen <peter.kelemen456@gmail.com>
2025-01-30 12:31:59 +0100

    Minor improvement

--- GameplayTimeTracker/UI/Menu/EditMenu.cs
+++ GameplayTimeTracker/UI/Menu/EditMenu.cs
@@ -1,16 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Media;
-using GameplayTimeTracker.Settings;
-using GameplayTimeTracker.SGDB;
-using Size = System.Drawing.Size;
 
 namespace GameplayTimeTracker.Menu;
 
@@ -36,12 +29,16 @@ public class EditMenu : EntryConfigMenu
         var RefreshLocalHeroButton =
             new CustomButton(w: 120, h: 40, text: "Local Refresh", effect: AppEffects.DropShadowIcon);
         RefreshLocalHeroButton.Margin = new Thickness(130, 20, 0, 30);
-        RefreshLocalHeroButton.Click += (_, __) =>
+        RefreshLocalHeroButton.Click += async (_, __) =>
         {
-            Guid guid = Guid.NewGuid();
-            string newImagePath = Path.Combine(AppFiles.SavedImagesPath, $"_{guid}.png");
-            ImageHelper.ScatterImage(entry.IconPath, newImagePath);
-            entry.HeroPath = newImagePath;
+            await Task.Run(() =>
+            {
+                Guid guid = Guid.NewGuid();
+                string newImagePath = Path.Combine(AppFiles.SavedImagesPath, $"_{guid}.png");
+                ImageHelper.ScatterImage(entry.IconPath, newImagePath);
+
+                Dispatcher.Invoke(() => entry.HeroPath = newImagePath);
+            });
         };
         refreshGrid.Children.Add(RefreshLocalHeroButton);
 
