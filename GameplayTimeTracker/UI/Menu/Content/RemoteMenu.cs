40f6907669da34c10b72674f3eee16d5c9f190b1
Péter Kelemen <peter.kelemen456@gmail.com>
2025-02-07 13:24:21 +0100

    Improved on remote menu layout

--- GameplayTimeTracker/UI/Menu/Content/RemoteMenu.cs
+++ GameplayTimeTracker/UI/Menu/Content/RemoteMenu.cs
@@ -10,6 +10,8 @@ namespace GameplayTimeTracker.Menu.Content;
 
 public class RemoteMenu : MenuContent
 {
+    private StackPanel RemoteConfigPanel;
+
     public RemoteMenu()
     {
         PrefEntry savingEnabledPref = new PrefEntry("Remote Saving Enabled", Common.Settings.IsRemoteSavingEnabled);
@@ -19,5 +21,37 @@ public class RemoteMenu : MenuContent
             savingEnabledBinding);
         BindingHelper.SetColorBinding(savingEnabledPref.textBlock, ForegroundProperty, "Font");
         _stackPanel.Children.Add(savingEnabledPref);
+
+        RemoteConfigPanel = new StackPanel();
+        _stackPanel.Children.Add(RemoteConfigPanel);
+
+        CreatePrefEntry("Address", "RemoteMachine.Address");
+        CreatePrefEntry("Port", "RemoteMachine.Port");
+        CreatePrefEntry("User", "RemoteMachine.User");
+        CreatePrefEntry("Password", "RemoteMachine.Password");
+        CreatePrefEntry("Remote Folder", "RemoteMachine.RemoteFolder", 20);
+    }
+
+    private void CreatePrefEntry(string blockText = "", string bindPath = "", double bottomMargin = 0)
+    {
+        StackPanel prefPanel = new StackPanel
+        {
+            Margin = new Thickness(0, 0, 0, bottomMargin),
+            HorizontalAlignment = HorizontalAlignment.Center,
+        };
+
+        TextBlock prefTextBlock =
+            UIHelper.CreateTextBlock(text: blockText, margin: new Thickness(5, 0, 0, 2), isBold: false,
+                vA: VerticalAlignment.Center);
+        BindingHelper.SetColorBinding(prefTextBlock, ForegroundProperty, "Font");
+
+        TextBox prefTextBox = UIHelper.CreateTextBox();
+        Binding boxBinding = new Binding(bindPath) { Source = Common.Settings, Mode = BindingMode.TwoWay, };
+        BindingOperations.SetBinding(prefTextBox, TextBox.TextProperty, boxBinding);
+        prefTextBox.Margin = new Thickness(0, 0, 0, 10);
+
+        prefPanel.Children.Add(prefTextBlock);
+        prefPanel.Children.Add(prefTextBox);
+        RemoteConfigPanel.Children.Add(prefPanel);
     }
 }
