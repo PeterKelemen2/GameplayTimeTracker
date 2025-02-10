db5ebd27f0be13ca1bcf71b9a3016f6db6a47b3b
Péter Kelemen <peter.kelemen456@gmail.com>
2025-02-10 12:35:02 +0100

    Improved on backup logic

--- GameplayTimeTracker/UI/Menu/Content/RemoteContent.cs
+++ GameplayTimeTracker/UI/Menu/Content/RemoteContent.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net.Mime;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
@@ -18,11 +21,14 @@ public class RemoteContent : UserControl
 {
     StackPanel savesStackPanel;
     StackPanel container;
+    CustomButton uploadButton;
+    CustomButton downloadButton;
     private Entry _entry;
     private List<string> filesList;
     private double scrollWidth = 300;
     private double scrollHeight = 200;
     private string currentSelectedPath = "";
+    private Run countRun;
 
     public RemoteContent()
     {
@@ -34,6 +40,12 @@ public class RemoteContent : UserControl
         filesList = new List<string>();
 
         container = new StackPanel();
+
+        TextBlock countBlock = UIHelper.CreateTextBlock("Saves found: ", fontSize: 17, hA: HorizontalAlignment.Center);
+        countRun = new Run { Text = "Calculating...", FontWeight = FontWeights.Regular };
+        countBlock.Inlines.Add(countRun);
+
+        container.Children.Add(countBlock);
 
         ScrollViewer savesScrollViewer = new ScrollViewer
             { Height = scrollHeight, Width = scrollWidth, VerticalScrollBarVisibility = ScrollBarVisibility.Hidden };
@@ -70,8 +82,8 @@ public class RemoteContent : UserControl
             { Orientation = Orientation.Horizontal, HorizontalAlignment = HorizontalAlignment.Center };
         Binding validPathBinding = new Binding("IsSavePathValid") { Source = _entry, Mode = BindingMode.OneWay };
 
-        var uploadButton = GetButton("Upload", UploadButton_Click, validPathBinding);
-        var downloadButton = GetButton("Download", DownloadButton_Click, validPathBinding);
+        uploadButton = GetButton("Upload", UploadButton_Click, validPathBinding);
+        downloadButton = GetButton("Download", DownloadButton_Click, validPathBinding);
 
         buttonsStackPanel.Children.Add(uploadButton);
         buttonsStackPanel.Children.Add(downloadButton);
@@ -91,19 +103,33 @@ public class RemoteContent : UserControl
 
     private async void DownloadButton_Click(object sender, RoutedEventArgs e)
     {
+        DisableButtons();
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Restart();
+
         if (string.IsNullOrEmpty(currentSelectedPath)) return;
         Console.WriteLine($"Downloading selected save from {currentSelectedPath}");
 
         bool success =
             await RemoteController.DownloadFolderAsync(currentSelectedPath, _entry.LocalSavePath);
         var downloadPrompt = new PromptMenu(width: 300,
-            textArray: new[] { success ? "Download successful!" : "Failed to download" }, boldArray: new[] { true },
+            textArray: new[] { success ? "Download successful!" : "Failed to download." }, boldArray: new[] { true },
             toScale: false);
         downloadPrompt.Open();
+
+        int elapsedMs = (int)Math.Round(stopwatch.Elapsed.TotalMilliseconds);
+        Console.WriteLine($"############ Download took {stopwatch.Elapsed.TotalMilliseconds.ToString("F2")}ms");
+        if (elapsedMs < 1000) await Task.Delay(1000 - elapsedMs);
+
+        EnableButtons();
     }
 
     private async void UploadButton_Click(object sender, RoutedEventArgs e)
     {
+        DisableButtons();
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Restart();
+
         if (!Directory.Exists(_entry.LocalSavePath)) return;
         Console.WriteLine($"Uploading local save to {_entry.LocalSavePath}");
 
@@ -111,11 +137,27 @@ public class RemoteContent : UserControl
             $"{Common.Settings.RemoteMachine.RemoteFolder.TrimEnd('/')}/{_entry.Name}/{DateTime.Now:yyyy-MM-dd-HH-mm-ss}";
         bool success = await RemoteController.UploadFolderAsync(_entry.LocalSavePath, uploadPath);
         var uploadPrompt = new PromptMenu(width: 300,
-            textArray: new[] { success ? "Upload successful!" : "Failed to upload" }, boldArray: new[] { true },
+            textArray: new[] { success ? "Upload successful!" : "Failed to upload." }, boldArray: new[] { true },
             toScale: false);
         uploadPrompt.Open();
 
         LoadData();
+        int elapsedMs = (int)Math.Round(stopwatch.Elapsed.TotalMilliseconds);
+        Console.WriteLine($"############ Upload took {stopwatch.Elapsed.TotalMilliseconds.ToString("F2")}ms");
+        if (elapsedMs < 1000) await Task.Delay(1000 - elapsedMs);
+        EnableButtons();
+    }
+
+    private void DisableButtons()
+    {
+        uploadButton.Active = false;
+        downloadButton.Active = false;
+    }
+
+    private void EnableButtons()
+    {
+        uploadButton.Active = true;
+        downloadButton.Active = true;
     }
 
     private async void AddLoadingIndicator()
@@ -145,11 +187,34 @@ public class RemoteContent : UserControl
             var remoteContent = await RemoteController
                 .ListGameSubfoldersAsync(Common.Settings.RemoteMachine.RemoteFolder, _entry.Name).ConfigureAwait(false);
 
-            Dispatcher.Invoke(() => { ShowSaves(remoteContent); });
+            Dispatcher.Invoke(() =>
+            {
+                ShowSaves(remoteContent);
+                // countRun.Text = remoteContent.Count.ToString();
+                ChangeRunText(remoteContent.Count.ToString());
+            });
         }
         catch (Exception ex)
         {
             Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+
+    private async Task ChangeRunText(string text)
+    {
+        int stepTime = (int)(AppAnimations.fastFadeAnimDuration / countRun.Text.Length * 1000);
+        Console.WriteLine($"Step time: {stepTime}ms");
+        while (countRun.Text.Length > 0)
+        {
+            countRun.Text = countRun.Text.Remove(countRun.Text.Length - 1, 1);
+            await Task.Delay(stepTime);
+        }
+
+        stepTime = (int)(AppAnimations.fastFadeAnimDuration / text.Length * 1000);
+        while (countRun.Text.Length < text.Length)
+        {
+            countRun.Text = countRun.Text.Insert(countRun.Text.Length, text[countRun.Text.Length].ToString());
+            await Task.Delay(stepTime);
         }
     }
 
