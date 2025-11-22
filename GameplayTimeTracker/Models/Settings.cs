e83fb4ff0c8e33f483d9a0ff93bc81a7db654687
PeterKelemen2 <peter.kelemen456@gmail.com>
2025-11-22 15:27:27 +0100

    Added base Settings class

---
+++ GameplayTimeTracker/Models/Settings.cs
@@ -0,0 +1,7 @@
+﻿namespace GameplayTimeTracker.Models;
+
+public class Settings : BaseDataModel
+{
+    public bool StartWithSystem { get; set; } = true;
+    public bool StartMinimized { get; set; } = false;
+}
