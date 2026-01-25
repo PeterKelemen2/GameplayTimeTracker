4d9d5629146c50f31301675fb90b50b751ead317
PeterKelemen2 <peter.kelemen456@gmail.com>
2026-01-25 15:07:31 +0100

    Improved on process tracking

---
+++ GameplayTimeTracker/Services/Tracker/GameTrackingManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using GameplayTimeTracker.Models;
+
+namespace GameplayTimeTracker.Services.Tracker;
+
+public class GameTrackingManager
+{
+    private readonly List<SingleProcessTracker> _trackers = new();
+
+    public void StartListening(IEnumerable<Game> games)
+    {
+        foreach (var game in games)
+        {
+            var tracker = new SingleProcessTracker(game);
+            tracker.Start();
+            _trackers.Add(tracker);
+        }
+    }
+
+    public void StopListening()
+    {
+        foreach (var tracker in _trackers)
+            tracker.Stop();
+
+        _trackers.Clear();
+    }
+}
---
+++ GameplayTimeTracker/Services/Tracker/SingleProcessTracker.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Management;
+using GameplayTimeTracker.Models;
+
+namespace GameplayTimeTracker.Services.Tracker;
+
+public class SingleProcessTracker
+{
+    private readonly Game _game;
+    private readonly HashSet<int> _runningProcesses = new();
+    private ManagementEventWatcher? _watcher;
+    
+    public SingleProcessTracker(Game game)
+    {
+        _game = game;
+    }
+
+    public void Start()
+    {
+        TrackExistingProcesses();
+
+        string query = "SELECT * FROM Win32_ProcessStartTrace";
+        _watcher = new ManagementEventWatcher(new WqlEventQuery(query));
+        _watcher.EventArrived += OnProcessStarted;
+        _watcher.Start();
+    }
+
+    private void TrackExistingProcesses()
+    {
+        var normalizedPath = Path.GetFullPath(_game.ExePath);
+
+        foreach (var process in Process.GetProcesses())
+        {
+            try
+            {
+                var path = process.MainModule?.FileName;
+                if (path != null && Path.GetFullPath(path).Equals(normalizedPath, StringComparison.OrdinalIgnoreCase))
+                    AttachProcess(process);
+            }
+            catch (Win32Exception)
+            {
+                // ignore access denied
+            }
+        }
+    }
+
+    private void OnProcessStarted(object sender, EventArrivedEventArgs e)
+    {
+        try
+        {
+            var processId = Convert.ToInt32(e.NewEvent.Properties["ProcessId"].Value);
+            var process = Process.GetProcessById(processId);
+
+            var path = process.MainModule?.FileName;
+            if (path != null && Path.GetFullPath(path).Equals(Path.GetFullPath(_game.ExePath), StringComparison.OrdinalIgnoreCase))
+                AttachProcess(process);
+        }
+        catch
+        {
+            // ignore errors
+        }
+    }
+
+    private void AttachProcess(Process process)
+    {
+        if (_runningProcesses.Contains(process.Id))
+            return;
+
+        _runningProcesses.Add(process.Id);
+
+        if (!_game.IsTracked)
+            StartSession(process.StartTime);
+
+        process.EnableRaisingEvents = true;
+        process.Exited += (_, _) =>
+        {
+            _runningProcesses.Remove(process.Id);
+
+            if (_runningProcesses.Count == 0)
+                EndSession(process.ExitTime);
+        };
+    }
+
+    private void StartSession(DateTime startTime)
+    {
+        _game.IsTracked = true;
+        _game.StartTime = startTime;
+        Console.WriteLine($"Session Start: {_game.DisplayName} - ({startTime})");
+    }
+
+    private void EndSession(DateTime endTime)
+    {
+        _game.IsTracked = false;
+        _game.EndTime = endTime;
+
+        var duration = PlaytimeCalcService.GetDurationFromDatesToString(_game.StartTime, _game.EndTime);
+        Console.WriteLine($"Session End: {_game.DisplayName} - ({endTime}) - Duration: {duration}");
+    }
+
+    public void Stop()
+    {
+        _watcher?.Stop();
+        _watcher?.Dispose();
+        _watcher = null;
+    }
+}
