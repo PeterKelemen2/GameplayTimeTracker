702cd4b83301b59654053c95c3c65df024506428
PeterKelemen2 <peter.kelemen456@gmail.com>
2026-02-01 17:38:47 +0100

    Added Theme getter to Settings Repository

--- GameplayTimeTracker/Repositories/SettingsRepository.cs
+++ GameplayTimeTracker/Repositories/SettingsRepository.cs
@@ -1,8 +1,15 @@
-﻿using GameplayTimeTracker.Data;
+﻿using System.Linq;
+using GameplayTimeTracker.Data;
 using GameplayTimeTracker.Models;
 
 namespace GameplayTimeTracker.Repositories;
 
 public class SettingsRepository(AppDbContext db) : Repository<Settings>(db)
 {
+    public Theme GetTheme()
+    {
+        int themeId = db.Settings.FirstOrDefault()?.ThemeId ?? 0;
+
+        return (themeId != 0 ? Db.Set<Theme>().Find(themeId) : new Theme()) ?? new Theme();
+    }
 }
