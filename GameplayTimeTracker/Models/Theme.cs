dfb36cc750b1110655c9002c1a62adf68d9ba92d
PeterKelemen2 <peter.kelemen456@gmail.com>
2026-02-01 17:38:16 +0100

    Added default value to Theme model

--- GameplayTimeTracker/Models/Theme.cs
+++ GameplayTimeTracker/Models/Theme.cs
@@ -4,23 +4,23 @@ namespace GameplayTimeTracker.Models;
 
 public class Theme : BaseDataModel
 {
-    public string ThemeName { get; set; } = "";
-    public string CFooter { get; set; } = "";
-    public string CFooterFont { get; set; } = "";
-    public string CBackground { get; set; } = "";
-    public string CCard1 { get; set; } = "";
-    public string CCard2 { get; set; } = "";
-    public string CProgressBar1 { get; set; } = "";
-    public string CProgressBar2 { get; set; } = "";
+    public string ThemeName { get; set; } = "Default Dark";
+    public string CFooter { get; set; } = "#6A6F99";
+    public string CFooterFont { get; set; } = "#DAE4FF";
+    public string CBackground { get; set; } = "#1E2030";
+    public string CCard1 { get; set; } = "#414769";
+    public string CCard2 { get; set; } = "#2E324A";
+    public string CProgressBar1 { get; set; } = "#89ACF2";
+    public string CProgressBar2 { get; set; } = "#B7BDF8";
     public ProgressBarType ProgressBarGradOrient { get; set; } = ProgressBarType.Horizontal;
-    public string CFont { get; set; } = "";
-    public string CRunningIndicator { get; set; } = "";
-    public string CButton { get; set; } = "";
-    public string CButtonFont { get; set; } = "";
-    public string CButtonPositive { get; set; } = "";
-    public string CButtonPositiveFont { get; set; } = "";
-    public string CButtonNegative { get; set; } = "";
-    [Display(Name = "Negative Button Font Color")] public string CButtonNegativeFont { get; set; } = "";
-    [Display(Name = "Shadow Color")] public string CShadow { get; set; } = "";
-    [Display(Name = "Transparency Effect Color")] public string CTransparency { get; set; } = "";
+    public string CFont { get; set; } = "#DAE4FF";
+    public string CRunningIndicator { get; set; } = "#C3E88D";
+    public string CButton { get; set; } = "#3BC9E3";
+    public string CButtonFont { get; set; } = "#DAE4FF";
+    public string CButtonPositive { get; set; } = "#90EE90";
+    public string CButtonPositiveFont { get; set; } = "#DAE4FF";
+    public string CButtonNegative { get; set; } = "#ED0C0C";
+    public string CButtonNegativeFont { get; set; } = "#DAE4FF";
+    public string CShadow { get; set; } = "#151515";
+    public string CTransparency { get; set; } = "#00000000";
 }
