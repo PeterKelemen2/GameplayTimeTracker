7fe2ce1248ce398e8850d6e4fea95c2b9bc1c3c0
PeterKelemen2 <peter.kelemen456@gmail.com>
2026-02-01 15:08:17 +0100

    Modified logging format

---
+++ GameplayTimeTracker/Converters/SimpleBracketConsoleFormatter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Logging.Console;
+
+namespace GameplayTimeTracker.Converters;
+
+public class SimpleBracketConsoleFormatter : ConsoleFormatter
+{
+    public SimpleBracketConsoleFormatter()
+        : base(nameof(SimpleBracketConsoleFormatter))
+    {
+    }
+
+    public override void Write<TState>(
+        in LogEntry<TState> logEntry,
+        IExternalScopeProvider? scopeProvider,
+        TextWriter textWriter)
+    {
+        var timestamp = DateTime.Now.ToString("yyyy.MM.dd-HH:mm:ss");
+        var category = logEntry.Category;
+        var eventId = logEntry.EventId.Id;
+        var message = logEntry.Formatter?.Invoke(logEntry.State, logEntry.Exception);
+
+        if (string.IsNullOrEmpty(message))
+            return;
+
+        textWriter.WriteLine(
+            $"[{category}[{eventId}]] [{timestamp}] - {message}");
+    }
+}
--- GameplayTimeTracker/Services/AppLogger.cs
+++ GameplayTimeTracker/Services/AppLogger.cs
@@ -1,16 +1,24 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using GameplayTimeTracker.Converters;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 
 namespace GameplayTimeTracker.Services;
 
 public static class AppLogger
 {
-    public static ILoggerFactory LoggerFactory { get; } = Microsoft.Extensions.Logging.LoggerFactory.Create(builder =>
-    {
-        builder
-            .AddConsole()
-            .AddDebug()
-            .SetMinimumLevel(LogLevel.Information); // default minimum level
-    });
+    public static ILoggerFactory LoggerFactory { get; }
+        = Microsoft.Extensions.Logging.LoggerFactory.Create(builder =>
+        {
+            builder
+                .ClearProviders()
+                .AddConsole(options =>
+                {
+                    options.FormatterName = nameof(SimpleBracketConsoleFormatter);
+                })
+                .AddConsoleFormatter<SimpleBracketConsoleFormatter, ConsoleFormatterOptions>()
+                .AddDebug()
+                .SetMinimumLevel(LogLevel.Information);
+        });
 
     public static ILogger<T> CreateLogger<T>() => LoggerFactory.CreateLogger<T>();
 }
